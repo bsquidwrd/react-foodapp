75446461b0a0a0b82a746c8b7aa8249b70786335
bsquidwrd <git@bsquid.io>
2024-02-27 04:21:09 +0000

    React For Beginners 48

---
+++ src/components/Search.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export default function Search() {
+  const [query, setQuery] = useState("pizza");
+  useEffect(() => {
+    function demo() {
+      console.log("Demo function executed");
+    }
+
+    demo();
+  }, [query]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </div>
+  );
+}
