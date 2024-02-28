b829d3bd60af6b1f71fed5e58889524bc563e3dd
bsquidwrd <git@bsquid.io>
2024-02-28 04:11:03 +0000

    Trying to make it prettier

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,11 +3,13 @@ import styles from "./itemlist.module.css";
 
 export default function ItemList({ items, isLoading }) {
   return (
-    <div>
+    <div className={styles.itemList}>
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        items.map((item) => <Item key={item.id} item={item} />)
+        items.map((item, index) => (
+          <Item key={`${index}-${item.id}`} item={item} />
+        ))
       )}
     </div>
   );
